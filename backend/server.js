a559183e48013849bb5a01020de4c1d66daca23e
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-07-29 13:44:56 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+import http from 'http';
+import app from './app.js';
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
