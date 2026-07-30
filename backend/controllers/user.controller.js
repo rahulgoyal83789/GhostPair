59b43efd783e008fda68d65205436fdd0e173ca3
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-07-30 18:49:41 +0530

    extra underscore found in user.controller.js fixed it , now login and register working properly

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,7 +12,7 @@ export const createUserController = async (req, res) => {
     try {
         const user = await userService.createUser(req.body);
         const token = await user.generateJWT();
-        delete user.__doc.password;
+        delete user._doc.password;
         res.status(201).json({ user, token });
     } catch (error) {
         res.status(400).send(error.message);
@@ -39,7 +39,7 @@ export const loginController = async (req, res) => {
         }
 
         const token = await user.generateJWT();
-        delete user.__doc.password;
+        delete user._doc.password;
         res.status(200).json({ user, token });
     } catch (error) {
         res.status(400).send(error.message);
